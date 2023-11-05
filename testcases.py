92e77cf665f34ad604260b7d1dba28733fa52a98
Hakerfire1234 <136265588+Hakerfire1234@users.noreply.github.com>
2023-11-05 10:53:35 -0500

    Update testcases.py

--- testcases.py
+++ testcases.py
@@ -1,29 +1,31 @@
-class Airport:
-    def __init__(self):
-        self.airport = []
+# class Airport:
+#     def __init__(self):
+#         self.airport = []
 
-    def flight(self, key):
-        if not self.inboard(key):
-            self.airport.append(key)
+#     def flight(self, key):
+#         if not self.inboard(key):
+#             self.airport.append(key)
 
-    def nextfly(self, key):
-        if self.inboard(key) and len(self.airport) > 1:
-            index = self.airport.index(key)
-            if index < len(self.airport) - 1:
-                return self.airport[index + 1]
-        return ValueError
+#     def nextfly(self, key):
+#         if self.inboard(key) and len(self.airport) > 1:
+#             index = self.airport.index(key)
+#             if index < len(self.airport) - 1:
+#                 return self.airport[index + 1]
+#         return ValueError
 
-    def inboard(self, key):
-        return key in self.airport
+#     def inboard(self, key):
+#         return key in self.airport
 
-    def showboard(self):
-        return self.airport
+#     def showboard(self):
+#         return self.airport
 
-    def removefly(self, key):
-      if self.inboard(key):
-        self.airport.remove(key)
-      else:
-        return ValueError
+#     def removefly(self, key):
+#       if self.inboard(key):
+#         self.airport.remove(key)
+#       else:
+#         return ValueError
+
+from airport import Airport
 
 def run_tests():
   airport = Airport()
@@ -43,6 +45,11 @@ def run_tests():
   # Remove a flight
   airport.removefly("Flight 1")
 
+  # Show updated flights
+  print(airport.showboard())
+
+# Run the tests
+run_tests()
   # Show updated flights
   print(airport.showboard())
 
