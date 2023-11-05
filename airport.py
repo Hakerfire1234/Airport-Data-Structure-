79c6998b538441018f127c21383adae8729e8f5c
Hakerfire1234 <136265588+Hakerfire1234@users.noreply.github.com>
2023-11-05 10:52:10 -0500

    Update airport.py

--- airport.py
+++ airport.py
@@ -1,19 +1,26 @@
 class Airport:
     def __init__(self):
         self.airport = []
-    
+
     def flight(self, key):
         if not self.inboard(key):
             self.airport.append(key)
 
     def nextfly(self, key):
-        if len(self.airport) > 1:
-            return self.airport[self.airport.index(key)+1]
-        else:
-            return None
-        
+        if self.inboard(key) and len(self.airport) > 1:
+            index = self.airport.index(key)
+            if index < len(self.airport) - 1:
+                return self.airport[index + 1]
+        return ValueError
+
     def inboard(self, key):
-        if key in self.airport:
-            return True
-        else:
-            return False
+        return key in self.airport
+
+    def showboard(self):
+        return self.airport
+
+    def removefly(self, key):
+      if self.inboard(key):
+        self.airport.remove(key)
+      else:
+        return ValueError
